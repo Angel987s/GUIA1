f825032a22079fea20699eb7483c979900315296
Angel Mena <angelmeba4@gmail.com>
2024-10-07 10:32:21 -0600

    Fin

---
+++ AMMA.TemperaturaConvert.UnitTest.AppMSTest/TemperatureConverteTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AMMA.Temperatura;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMMA.Temperatura.Tests
+{
+    [TestClass()]
+    public class TemperatureConverteTests
+    {
+        [TestMethod()]
+        public void CelsiusToFahrenheitTest()
+        {
+            TemperatureConverte converter = new TemperatureConverte();
+
+            double result = converter.CelsiusToFahrenheit(0);
+            Assert.AreEqual(32, result, 0.001); // Prueba 0°C = 32°F
+
+         
+            
+        }
+
+        [TestMethod()]
+        public void FahrenheitToCelsiusTest()
+        {
+            TemperatureConverte converter = new TemperatureConverte();
+
+            double result = converter.FahrenheitToCelsius(32);
+            Assert.AreEqual(0, result, 0.001); // Prueba 32°F = 0°C
+
+           
+        }
+    }
+}
---
+++ AMMA.TemperaturaConvert.UnitTest.AppMSTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace AMMA.TemperaturaConvert.UnitTest.AppMSTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
